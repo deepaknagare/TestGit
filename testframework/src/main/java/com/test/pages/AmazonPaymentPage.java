311d7c63cc3f14e4cf1325c79586695fa40aa9bb
Deepak Nagare <deepak.nagare@amdocs.com>
2019-05-21 20:33:27 +0530

    pushing tests to git

---
+++ testframework/src/main/java/com/test/pages/AmazonPaymentPage.java
@@ -0,0 +1,17 @@
+package com.test.pages;
+
+import com.test.testbase.TestBase;
+
+public class AmazonPaymentPage extends TestBase {
+
+	
+	public AmazonPaymentPage() {
+
+	
+	}
+	
+	public void selectPaymentOption()
+	{
+		
+	}
+}
